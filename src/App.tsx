6081c6253c4a146664ff7a375321591f38474bec
Kostya Ekimenko <fantasigrape@mail.ru>
2023-09-05 16:59:39 +0300

    подключили route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
