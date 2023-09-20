fdbaf98a3520f9fd6ea9b5b286d8c2fe706fb3bb
Kostya Ekimenko <fantasigrape@mail.ru>
2023-09-20 21:23:05 +0300

    refactor: utills

--- src/utils/getVinylsForFavorite.ts
+++ src/utils/getVinylsForFavorite.ts
@@ -2,9 +2,16 @@ import { ResponseParams } from "../redux/favoritesApi";
 import { Vinyl } from "../pages/Home/Home";
 
 type FavoritesById = {
-  [uniqueId: string]: Vinyl;
+  uniqueId: Vinyl;
 };
 
+/**
+ * Спускаемся во вложенный объект чтобы получить результат
+ * Example:
+ * {id: {uniqueId: {vinyl}} => [{vinyl},{vinyl}...]
+ * @param data
+ * @returns Vinyl[]
+ */
 export function getVinylsForFavorite(data: ResponseParams) {
   const res: FavoritesById[] = Object.values(data);
   return res.map((vinyl) => Object.values(vinyl)[0]);
